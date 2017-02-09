e8291a7987b3f6269c325c70100e1af096b9e0a6
konstantinoskr <contact@ikonstantinos.com>
2017-02-09 16:34:38 +0000

    read files from delta::sys

---
+++ delta_peano/delta/sys/read.cpp
@@ -0,0 +1,13 @@
+/*
+ * read.cpp
+ *
+ *  Created on: 8 Feb 2017
+ *      Author: konstantinos
+ */
+
+#include "read.h"
+
+void delta::sys::parseModelGridSchematics(std::string inputfile, std::vector<std::vector<int>> &componentGrid)
+{
+
+}
---
+++ delta_peano/delta/sys/read.h
@@ -0,0 +1,20 @@
+/*
+ * read.h
+ *
+ *  Created on: 8 Feb 2017
+ *      Author: konstantinos
+ */
+
+#ifndef DELTA_SYS_READ_H_
+#define DELTA_SYS_READ_H_
+
+#include <string>
+#include <iostream>
+
+namespace delta {
+	namespace sys {
+		void parseModelGridSchematics(std::string inputfile, std::vector<std::vector<int>> &componentGrid);
+	}
+}
+
+#endif /* DELTA_SYS_READ_H_ */
