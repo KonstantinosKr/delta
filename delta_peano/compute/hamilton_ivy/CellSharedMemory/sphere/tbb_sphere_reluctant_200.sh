87ec7b9645f5ecc0be4fbe6f42a809c22ece6b08
konstantinoskr <contact@ikonstantinos.com>
2017-02-09 22:04:34 +0000

    sphere with cell based-tbb scripts

---
+++ delta_peano/compute/hamilton_ivy/CellSharedMemory/sphere/tbb_sphere_reluctant_200.sh
@@ -0,0 +1,79 @@
+#!/bin/bash
+###############################################################################
+# Submits a job.
+###############################################################################
+# Is invoked with the following arguments
+# $1 path to the output file
+
+JOB_SCRIPT0=runjobBatch1
+JOB_SCRIPT1=runjobBatch2
+JOB_SCRIPT2=runjobBatch3
+JOB_SCRIPT3=runjobBatch4
+JOB_SCRIPT4=runjobBatch6
+JOB_SCRIPT5=runjobBatch8
+JOB_SCRIPT6=runjobBatch16
+
+exp0o=/scratch/rfmw74/reluctantG4SphereTBB1_200
+exp1o=/scratch/rfmw74/reluctantG4SphereTBB2_200
+exp2o=/scratch/rfmw74/reluctantG4SphereTBB3_200
+exp3o=/scratch/rfmw74/reluctantG4SphereTBB4_200
+exp4o=/scratch/rfmw74/reluctantG4SphereTBB6_200
+exp5o=/scratch/rfmw74/reluctantG4SphereTBB8_200
+exp6o=/scratch/rfmw74/reluctantG4SphereTBB16_200
+
+exp0="./dem-3d-release-tbb-omp-triangle 0.3 0.3 0.003 freefall 5000 reluctant-adaptive-grid 0.0001 never 1 10 sphere 200 1"
+exp1="./dem-3d-release-tbb-omp-triangle 0.3 0.3 0.003 freefall 5000 reluctant-adaptive-grid 0.0001 never 1 10 sphere 200 2"
+exp2="./dem-3d-release-tbb-omp-triangle 0.3 0.3 0.003 freefall 5000 reluctant-adaptive-grid 0.0001 never 1 10 sphere 200 3"
+exp3="./dem-3d-release-tbb-omp-triangle 0.3 0.3 0.003 freefall 5000 reluctant-adaptive-grid 0.0001 never 1 10 sphere 200 4"
+exp4="./dem-3d-release-tbb-omp-triangle 0.3 0.3 0.003 freefall 5000 reluctant-adaptive-grid 0.0001 never 1 10 sphere 200 6"
+exp5="./dem-3d-release-tbb-omp-triangle 0.3 0.3 0.003 freefall 5000 reluctant-adaptive-grid 0.0001 never 1 10 sphere 200 8"
+exp6="./dem-3d-release-tbb-omp-triangle 0.3 0.3 0.003 freefall 5000 reluctant-adaptive-grid 0.0001 never 1 10 sphere 200 16"
+
+sed "s,{OUTPUT_FILE},$exp0o,g" ${JOB_SCRIPT0} > ${JOB_SCRIPT0}_tmp
+sed "s,{RUN},$exp0,g" ${JOB_SCRIPT0}_tmp > ${JOB_SCRIPT0}_tmp1
+sbatch ${JOB_SCRIPT0}_tmp1
+
+rm ${JOB_SCRIPT0}_tmp
+rm ${JOB_SCRIPT0}_tmp1
+
+sed "s,{OUTPUT_FILE},$exp1o,g" ${JOB_SCRIPT1} > ${JOB_SCRIPT1}_tmp
+sed "s,{RUN},$exp1,g" ${JOB_SCRIPT1}_tmp > ${JOB_SCRIPT1}_tmp1
+sbatch ${JOB_SCRIPT1}_tmp1
+
+rm ${JOB_SCRIPT1}_tmp
+rm ${JOB_SCRIPT1}_tmp1
+
+sed "s,{OUTPUT_FILE},$exp2o,g" ${JOB_SCRIPT2} > ${JOB_SCRIPT2}_tmp
+sed "s,{RUN},$exp2,g" ${JOB_SCRIPT2}_tmp > ${JOB_SCRIPT2}_tmp1
+sbatch ${JOB_SCRIPT2}_tmp1
+
+rm ${JOB_SCRIPT2}_tmp
+rm ${JOB_SCRIPT2}_tmp1
+
+sed "s,{OUTPUT_FILE},$exp3o,g" ${JOB_SCRIPT3} > ${JOB_SCRIPT3}_tmp
+sed "s,{RUN},$exp3,g" ${JOB_SCRIPT3}_tmp > ${JOB_SCRIPT3}_tmp1
+sbatch ${JOB_SCRIPT3}_tmp1
+
+rm ${JOB_SCRIPT3}_tmp
+rm ${JOB_SCRIPT3}_tmp1
+
+sed "s,{OUTPUT_FILE},$exp4o,g" ${JOB_SCRIPT4} > ${JOB_SCRIPT4}_tmp
+sed "s,{RUN},$exp4,g" ${JOB_SCRIPT4}_tmp > ${JOB_SCRIPT4}_tmp1
+sbatch ${JOB_SCRIPT4}_tmp1
+
+rm ${JOB_SCRIPT4}_tmp
+rm ${JOB_SCRIPT4}_tmp1
+
+sed "s,{OUTPUT_FILE},$exp5o,g" ${JOB_SCRIPT5} > ${JOB_SCRIPT5}_tmp
+sed "s,{RUN},$exp5,g" ${JOB_SCRIPT5}_tmp > ${JOB_SCRIPT5}_tmp1
+sbatch ${JOB_SCRIPT5}_tmp1
+
+rm ${JOB_SCRIPT5}_tmp
+rm ${JOB_SCRIPT5}_tmp1
+
+sed "s,{OUTPUT_FILE},$exp6o,g" ${JOB_SCRIPT5} > ${JOB_SCRIPT5}_tmp
+sed "s,{RUN},$exp6,g" ${JOB_SCRIPT5}_tmp > ${JOB_SCRIPT5}_tmp1
+sbatch ${JOB_SCRIPT5}_tmp1
+
+rm ${JOB_SCRIPT5}_tmp
+rm ${JOB_SCRIPT5}_tmp1
