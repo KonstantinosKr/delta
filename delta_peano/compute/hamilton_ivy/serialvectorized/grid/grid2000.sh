82a37f31675d7d841070d6f83311c44246c57869
KRESTENITIS <rfmw74@hamilton2.dur.ac.uk>
2017-02-28 02:04:49 +0000

    grid scripts

--- delta_peano/compute/hamilton_ivy/serialvectorized/grid/grid2000.sh
+++ delta_peano/compute/hamilton_ivy/serialvectorized/grid/grid2000.sh
@@ -7,13 +7,13 @@
 
 JOB_SCRIPT=runjobBatch
 
-exp1o=/scratch/rfmw74/regularGridcmp20
-exp2o=/scratch/rfmw74/adaptiveGridcmp20
-exp3o=/scratch/rfmw74/reluctantGridcmp20
+exp1o=/scratch/rfmw74/regularGridcmp2000
+exp2o=/scratch/rfmw74/adaptiveGridcmp2000
+exp3o=/scratch/rfmw74/reluctantGridcmp2000
 
-exp1="./dem-3d-release-vec 0.3 0.003 0.3 freefall 5000 regular-grid 0.0001 never 10 1 bf 20”
-exp2="./dem-3d-release-vec 0.3 0.003 0.3 freefall 5000 adaptive-grid 0.0001 never 10 1 bf 20”
-exp3="./dem-3d-release-vec 0.3 0.003 0.3 freefall 5000 reluctant-adaptive-grid 0.0001 never 10 1 bf 20”
+exp1="./dem-3d-release-vec 0.3 0.003 0.3 freefall 5000 regular-grid 0.0001 never 10 1 bf 2000"
+exp2="./dem-3d-release-vec 0.3 0.003 0.3 freefall 5000 adaptive-grid 0.0001 never 10 1 bf 2000"
+exp3="./dem-3d-release-vec 0.3 0.003 0.3 freefall 5000 reluctant-adaptive-grid 0.0001 never 10 1 bf 2000"
 
 sed "s,{OUTPUT_FILE},$exp0o,g" ${JOB_SCRIPT} > ${JOB_SCRIPT}_tmp
 sed "s,{RUN},$exp0,g" ${JOB_SCRIPT}_tmp > ${JOB_SCRIPT}_tmp1
