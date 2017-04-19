f7dbf288ecb217e853aa268e7eb7c900b0156e34
konstantinoskr <contact@ikonstantinos.com>
2017-04-19 17:41:39 +0100

    10k hopper

--- delta_peano/compute/hamilton7_knl/hopper/10000/scale200/omptriangle_bf.sh
+++ delta_peano/compute/hamilton7_knl/hopper/10000/scale200/omptriangle_bf.sh
@@ -14,23 +14,23 @@ JOB_SCRIPT5=runjobBatch8
 JOB_SCRIPT6=runjobBatch16
 JOB_SCRIPT7=runjobBatch24
 
-exp0o=/ddn/data/rfmw74/h7ScaleregularpenaltyOMPTRIANGLE1_50
-exp1o=/ddn/data/rfmw74/h7ScaleregularpenaltyOMPTRIANGLE2_50
-exp2o=/ddn/data/rfmw74/h7ScaleregularpenaltyOMPTRIANGLE3_50
-exp3o=/ddn/data/rfmw74/h7ScaleregularpenaltyOMPTRIANGLE4_50
-exp4o=/ddn/data/rfmw74/h7ScaleregularpenaltyOMPTRIANGLE6_50
-exp5o=/ddn/data/rfmw74/h7ScaleregularpenaltyOMPTRIANGLE8_50
-exp6o=/ddn/data/rfmw74/h7ScaleregularpenaltyOMPTRIANGLE16_50
-exp7o=/ddn/data/rfmw74/h7ScaleregularpenaltyOMPTRIANGLE24_50
+exp0o=/ddn/data/rfmw74/h7ScaleregularbfOMPTRIANGLE1_200
+exp1o=/ddn/data/rfmw74/h7ScaleregularbfOMPTRIANGLE2_200
+exp2o=/ddn/data/rfmw74/h7ScaleregularbfOMPTRIANGLE3_200
+exp3o=/ddn/data/rfmw74/h7ScaleregularbfOMPTRIANGLE4_200
+exp4o=/ddn/data/rfmw74/h7ScaleregularbfOMPTRIANGLE6_200
+exp5o=/ddn/data/rfmw74/h7ScaleregularbfOMPTRIANGLE8_200
+exp6o=/ddn/data/rfmw74/h7ScaleregularbfOMPTRIANGLE16_200
+exp7o=/ddn/data/rfmw74/h7ScaleregularbfOMPTRIANGLE24_200
 
-exp0="./dem-3d-release-omp-triangle 0.3 0.003 0.3 hopperUniformMesh10k 100 regular-grid 0.00001 never 10 1 penalty 50"
-exp1="./dem-3d-release-omp-triangle 0.3 0.003 0.3 hopperUniformMesh10k 100 regular-grid 0.00001 never 10 1 penalty 50"
-exp2="./dem-3d-release-omp-triangle 0.3 0.003 0.3 hopperUniformMesh10k 100 regular-grid 0.00001 never 10 1 penalty 50"
-exp3="./dem-3d-release-omp-triangle 0.3 0.003 0.3 hopperUniformMesh10k 100 regular-grid 0.00001 never 10 1 penalty 50"
-exp4="./dem-3d-release-omp-triangle 0.3 0.003 0.3 hopperUniformMesh10k 100 regular-grid 0.00001 never 10 1 penalty 50"
-exp5="./dem-3d-release-omp-triangle 0.3 0.003 0.3 hopperUniformMesh10k 100 regular-grid 0.00001 never 10 1 penalty 50"
-exp6="./dem-3d-release-omp-triangle 0.3 0.003 0.3 hopperUniformMesh10k 100 regular-grid 0.00001 never 10 1 penalty 50"
-exp7="./dem-3d-release-omp-triangle 0.3 0.003 0.3 hopperUniformMesh10k 100 regular-grid 0.00001 never 10 1 penalty 50"
+exp0="./dem-3d-release-omp-triangle 0.3 0.003 0.3 hopperUniformMesh10k 100 regular-grid 0.00001 never 10 1 bf 200"
+exp1="./dem-3d-release-omp-triangle 0.3 0.003 0.3 hopperUniformMesh10k 100 regular-grid 0.00001 never 10 1 bf 200"
+exp2="./dem-3d-release-omp-triangle 0.3 0.003 0.3 hopperUniformMesh10k 100 regular-grid 0.00001 never 10 1 bf 200"
+exp3="./dem-3d-release-omp-triangle 0.3 0.003 0.3 hopperUniformMesh10k 100 regular-grid 0.00001 never 10 1 bf 200"
+exp4="./dem-3d-release-omp-triangle 0.3 0.003 0.3 hopperUniformMesh10k 100 regular-grid 0.00001 never 10 1 bf 200"
+exp5="./dem-3d-release-omp-triangle 0.3 0.003 0.3 hopperUniformMesh10k 100 regular-grid 0.00001 never 10 1 bf 200"
+exp6="./dem-3d-release-omp-triangle 0.3 0.003 0.3 hopperUniformMesh10k 100 regular-grid 0.00001 never 10 1 bf 200"
+exp7="./dem-3d-release-omp-triangle 0.3 0.003 0.3 hopperUniformMesh10k 100 regular-grid 0.00001 never 10 1 bf 200"
 
 sed "s,{OUTPUT_FILE},$exp0o,g" ${JOB_SCRIPT0} > ${JOB_SCRIPT0}_tmp
 sed "s,{RUN},$exp0,g" ${JOB_SCRIPT0}_tmp > ${JOB_SCRIPT0}_tmp1
