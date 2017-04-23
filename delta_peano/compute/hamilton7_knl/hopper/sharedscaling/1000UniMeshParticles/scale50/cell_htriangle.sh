2c204a6a126af8ae6915dea3b325b884880199cb
konstantinoskr <contact@ikonstantinos.com>
2017-04-23 17:31:52 +0100

    minor change

--- delta_peano/compute/hamilton7_knl/hopper/sharedscaling/1000UniMeshParticles/scale50/cell_htriangle.sh
+++ delta_peano/compute/hamilton7_knl/hopper/sharedscaling/1000UniMeshParticles/scale50/cell_htriangle.sh
@@ -25,15 +25,15 @@ exp6o=/ddn/data/rfmw74/h7Scaleregularhybrid-on-triangle-pairsCELLTBB12_50_50hopp
 exp7o=/ddn/data/rfmw74/h7Scaleregularhybrid-on-triangle-pairsCELLTBB16_50_50hopper
 exp8o=/ddn/data/rfmw74/h7Scaleregularhybrid-on-triangle-pairsCELLTBB24_50_50hopper
 
-exp0="./dem-3d-release-tbb-triangle 0.3 0.003 0.3 hopperUniformMesh1k 5 regular-grid 0.00001 never 10 1 hybrid-on-triangle-pairs 50 1”
-exp1="./dem-3d-release-tbb-triangle 0.3 0.003 0.3 hopperUniformMesh1k 5 regular-grid 0.00001 never 10 1 hybrid-on-triangle-pairs 50 2”
-exp2="./dem-3d-release-tbb-triangle 0.3 0.003 0.3 hopperUniformMesh1k 5 regular-grid 0.00001 never 10 1 hybrid-on-triangle-pairs 50 3”
-exp3="./dem-3d-release-tbb-triangle 0.3 0.003 0.3 hopperUniformMesh1k 5 regular-grid 0.00001 never 10 1 hybrid-on-triangle-pairs 50 4”
-exp4="./dem-3d-release-tbb-triangle 0.3 0.003 0.3 hopperUniformMesh1k 5 regular-grid 0.00001 never 10 1 hybrid-on-triangle-pairs 50 6”
-exp5="./dem-3d-release-tbb-triangle 0.3 0.003 0.3 hopperUniformMesh1k 5 regular-grid 0.00001 never 10 1 hybrid-on-triangle-pairs 50 8”
-exp6="./dem-3d-release-tbb-triangle 0.3 0.003 0.3 hopperUniformMesh1k 5 regular-grid 0.00001 never 10 1 hybrid-on-triangle-pairs 50 12”
-exp7="./dem-3d-release-tbb-triangle 0.3 0.003 0.3 hopperUniformMesh1k 5 regular-grid 0.00001 never 10 1 hybrid-on-triangle-pairs 50 16”
-exp8="./dem-3d-release-tbb-triangle 0.3 0.003 0.3 hopperUniformMesh1k 5 regular-grid 0.00001 never 10 1 hybrid-on-triangle-pairs 50 24”
+exp0="./dem-3d-release-tbb 0.3 0.003 0.3 hopperUniformMesh1k 5 regular-grid 0.00001 never 10 1 hybrid-on-triangle-pairs 50 1"
+exp1="./dem-3d-release-tbb 0.3 0.003 0.3 hopperUniformMesh1k 5 regular-grid 0.00001 never 10 1 hybrid-on-triangle-pairs 50 2"
+exp2="./dem-3d-release-tbb 0.3 0.003 0.3 hopperUniformMesh1k 5 regular-grid 0.00001 never 10 1 hybrid-on-triangle-pairs 50 3"
+exp3="./dem-3d-release-tbb 0.3 0.003 0.3 hopperUniformMesh1k 5 regular-grid 0.00001 never 10 1 hybrid-on-triangle-pairs 50 4"
+exp4="./dem-3d-release-tbb 0.3 0.003 0.3 hopperUniformMesh1k 5 regular-grid 0.00001 never 10 1 hybrid-on-triangle-pairs 50 6"
+exp5="./dem-3d-release-tbb 0.3 0.003 0.3 hopperUniformMesh1k 5 regular-grid 0.00001 never 10 1 hybrid-on-triangle-pairs 50 8"
+exp6="./dem-3d-release-tbb 0.3 0.003 0.3 hopperUniformMesh1k 5 regular-grid 0.00001 never 10 1 hybrid-on-triangle-pairs 50 12"
+exp7="./dem-3d-release-tbb 0.3 0.003 0.3 hopperUniformMesh1k 5 regular-grid 0.00001 never 10 1 hybrid-on-triangle-pairs 50 16"
+exp8="./dem-3d-release-tbb 0.3 0.003 0.3 hopperUniformMesh1k 5 regular-grid 0.00001 never 10 1 hybrid-on-triangle-pairs 50 24"
 
 sed "s,{OUTPUT_FILE},$exp0o,g" ${JOB_SCRIPT0} > ${JOB_SCRIPT0}_tmp
 sed "s,{RUN},$exp0,g" ${JOB_SCRIPT0}_tmp > ${JOB_SCRIPT0}_tmp1
@@ -92,7 +92,7 @@ rm ${JOB_SCRIPT7}_tmp
 rm ${JOB_SCRIPT7}_tmp1
 
 sed "s,{OUTPUT_FILE},$exp8o,g" ${JOB_SCRIPT8} > ${JOB_SCRIPT8}_tmp
-sed "s,{RUN},$exp8,g” ${JOB_SCRIPT8}_tmp > ${JOB_SCRIPT8}_tmp1
+sed "s,{RUN},$exp8,g" ${JOB_SCRIPT8}_tmp > ${JOB_SCRIPT8}_tmp1
 sbatch ${JOB_SCRIPT8}_tmp1
 
 rm ${JOB_SCRIPT8}_tmp
