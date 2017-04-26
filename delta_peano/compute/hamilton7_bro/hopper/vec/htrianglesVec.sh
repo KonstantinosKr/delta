a5871025dfe8f463f44357efd66bf00cbccd855c
konstantinoskr <contact@ikonstantinos.com>
2017-04-26 18:46:37 +0100

    novec/vec

---
+++ delta_peano/compute/hamilton7_bro/hopper/vec/htrianglesVec.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+###############################################################################
+# Submits a job.
+###############################################################################
+# Is invoked with the following arguments
+# $1 path to the output file
+
+JOB_SCRIPT0=runjobBatch
+
+exp0o=/ddn/data/rfmw74/h7regularhybrid-on-triangle-pairsnovec1_2000
+
+exp0="./dem-3d-release-novec 0.3 0.003 0.3 freefall 1000 regular-grid 0.0001 never 10 1 hybrid-on-triangle-pairs 2000"
+
+sed "s,{OUTPUT_FILE},$exp0o,g" ${JOB_SCRIPT0} > ${JOB_SCRIPT0}_tmp
+sed "s,{RUN},$exp0,g" ${JOB_SCRIPT0}_tmp > ${JOB_SCRIPT0}_tmp1
+sbatch ${JOB_SCRIPT0}_tmp1
+
+rm ${JOB_SCRIPT0}_tmp
+rm ${JOB_SCRIPT0}_tmp1
