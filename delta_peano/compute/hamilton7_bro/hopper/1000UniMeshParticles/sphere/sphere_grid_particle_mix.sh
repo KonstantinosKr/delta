58d4fd9ebbe2d8fb4a9555d1b36b93afc1323fae
konstantinoskr <contact@ikonstantinos.com>
2017-04-27 08:29:00 +0100

    exec rename

--- delta_peano/compute/hamilton7_bro/hopper/1000UniMeshParticles/sphere/sphere_grid_particle_mix.sh
+++ delta_peano/compute/hamilton7_bro/hopper/1000UniMeshParticles/sphere/sphere_grid_particle_mix.sh
@@ -16,9 +16,9 @@ exp0o=/ddn/data/rfmw74/h7X21hopperUniMesh1k-regular-sphere-mesh10-tbb16omp2
 exp1o=/ddn/data/rfmw74/h7X22hopperUniMesh1k-adaptive-sphere-mesh10-tbb16omp2
 exp2o=/ddn/data/rfmw74/h7X23hopperUniMesh1k-reluctant-sphere-mesh10-tbb16omp2
 
-exp0="./dem-3d-release-tbb 0.3 0.003 0.3 hopperUniformMesh1k 1000 regular-grid 0.00003 never 10 1 sphere 10 16"
-exp1="./dem-3d-release-tbb 0.3 0.003 0.3 hopperUniformMesh1k 1000 adaptive-grid 0.00003 never 10 1 sphere 10 16"
-exp2="./dem-3d-release-tbb 0.3 0.003 0.3 hopperUniformMesh1k 1000 reluctant-adaptive-grid 0.00003 never 10 1 sphere 10 16"
+exp0="./dem-3d-release-tbb-omp-particle 0.3 0.003 0.3 hopperUniformMesh1k 1000 regular-grid 0.00003 never 10 1 sphere 10 16"
+exp1="./dem-3d-release-tbb-omp-particle 0.3 0.003 0.3 hopperUniformMesh1k 1000 adaptive-grid 0.00003 never 10 1 sphere 10 16"
+exp2="./dem-3d-release-tbb-omp-particle 0.3 0.003 0.3 hopperUniformMesh1k 1000 reluctant-adaptive-grid 0.00003 never 10 1 sphere 10 16"
 
 sed "s,{OUTPUT_FILE},$exp0o,g" ${JOB_SCRIPT0} > ${JOB_SCRIPT0}_tmp
 sed "s,{RUN},$exp0,g" ${JOB_SCRIPT0}_tmp > ${JOB_SCRIPT0}_tmp1
