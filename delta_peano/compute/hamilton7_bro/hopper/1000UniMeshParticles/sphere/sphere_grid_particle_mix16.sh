9ee84e96e20f675e71e5896ed0fbd3489a7cf610
konstantinoskr <contact@ikonstantinos.com>
2017-04-27 16:38:58 +0100

    12 threads plus 2

--- delta_peano/compute/hamilton7_bro/hopper/1000UniMeshParticles/sphere/sphere_grid_particle_mix16.sh
+++ delta_peano/compute/hamilton7_bro/hopper/1000UniMeshParticles/sphere/sphere_grid_particle_mix16.sh
@@ -5,17 +5,17 @@
 # Is invoked with the following arguments
 # $1 path to the output file
 
-JOB_SCRIPT0=runjobBatch24tbb16omp2
-JOB_SCRIPT1=runjobBatch24tbb16omp2
-JOB_SCRIPT2=runjobBatch24tbb16omp2
+JOB_SCRIPT0=runjobBatch24tbb12omp2
+JOB_SCRIPT1=runjobBatch24tbb12omp2
+JOB_SCRIPT2=runjobBatch24tbb12omp2
 
-exp0o=/ddn/data/rfmw74/h7X21hopperUniMesh1k-regular-sphere-mesh10-tbb16omp2
-exp1o=/ddn/data/rfmw74/h7X22hopperUniMesh1k-adaptive-sphere-mesh10-tbb16omp2
-exp2o=/ddn/data/rfmw74/h7X23hopperUniMesh1k-reluctant-sphere-mesh10-tbb16omp2
+exp0o=/ddn/data/rfmw74/h7X21hopperUniMesh1k-regular-sphere-mesh10-tbb12omp2
+exp1o=/ddn/data/rfmw74/h7X22hopperUniMesh1k-adaptive-sphere-mesh10-tbb12omp2
+exp2o=/ddn/data/rfmw74/h7X23hopperUniMesh1k-reluctant-sphere-mesh10-tbb12omp2
 
-exp0="./dem-3d-release-tbb-omp-particle 0.3 0.003 0.3 hopperUniformMesh1k 1000 regular-grid 0.00003 never 10 1 sphere 10 16"
-exp1="./dem-3d-release-tbb-omp-particle 0.3 0.003 0.3 hopperUniformMesh1k 1000 adaptive-grid 0.00003 never 10 1 sphere 10 16"
-exp2="./dem-3d-release-tbb-omp-particle 0.3 0.003 0.3 hopperUniformMesh1k 1000 reluctant-adaptive-grid 0.00003 never 10 1 sphere 10 16"
+exp0="./dem-3d-release-tbb-omp-particle 0.3 0.003 0.3 hopperUniformMesh1k 1000 regular-grid 0.00003 never 10 1 sphere 10 12"
+exp1="./dem-3d-release-tbb-omp-particle 0.3 0.003 0.3 hopperUniformMesh1k 1000 adaptive-grid 0.00003 never 10 1 sphere 10 12"
+exp2="./dem-3d-release-tbb-omp-particle 0.3 0.003 0.3 hopperUniformMesh1k 1000 reluctant-adaptive-grid 0.00003 never 10 1 sphere 10 12"
 
 sed "s,{OUTPUT_FILE},$exp0o,g" ${JOB_SCRIPT0} > ${JOB_SCRIPT0}_tmp
 sed "s,{RUN},$exp0,g" ${JOB_SCRIPT0}_tmp > ${JOB_SCRIPT0}_tmp1
