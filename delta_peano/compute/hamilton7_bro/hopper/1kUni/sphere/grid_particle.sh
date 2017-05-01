373bd486339764a585ade1fc4e824461c09eacd0
konstantinoskr <contact@ikonstantinos.com>
2017-05-01 10:44:05 +0100

    enabled autotuning

--- delta_peano/compute/hamilton7_bro/hopper/1kUni/sphere/grid_particle.sh
+++ delta_peano/compute/hamilton7_bro/hopper/1kUni/sphere/grid_particle.sh
@@ -5,26 +5,26 @@
 # Is invoked with the following arguments
 # $1 path to the output file
 
-JOB_SCRIPT0=runjobBatch2
-JOB_SCRIPT1=runjobBatch2
-JOB_SCRIPT2=runjobBatch2
-JOB_SCRIPT3=runjobBatch2
-JOB_SCRIPT4=runjobBatch2
-JOB_SCRIPT5=runjobBatch2
+JOB_SCRIPT0=runjobBatch24
+JOB_SCRIPT1=runjobBatch24
+JOB_SCRIPT2=runjobBatch24
+JOB_SCRIPT3=runjobBatch24
+JOB_SCRIPT4=runjobBatch24
+JOB_SCRIPT5=runjobBatch24
 
-exp0o=/ddn/data/rfmw74/h7X21hopperUni1k-regular-sphere-mesh10-tbb
-exp1o=/ddn/data/rfmw74/h7X22hopperUni1k-adaptive-sphere-mesh10-tbb
-exp2o=/ddn/data/rfmw74/h7X23hopperUni1k-reluctant-sphere-mesh10-tbb
-exp3o=/ddn/data/rfmw74/h7X24hopperUni1k-regular-sphere-mesh10-particle
-exp4o=/ddn/data/rfmw74/h7X25hopperUni1k-adaptive-sphere-mesh10-particle
-exp5o=/ddn/data/rfmw74/h7X26hopperUni1k-reluctant-sphere-mesh10-particle
+exp0o=/ddn/data/rfmw74/h7X21hopperUni1k-regular-sphere-mesh10-tbb24
+exp1o=/ddn/data/rfmw74/h7X22hopperUni1k-adaptive-sphere-mesh10-tbb24
+exp2o=/ddn/data/rfmw74/h7X23hopperUni1k-reluctant-sphere-mesh10-tbb24
+exp3o=/ddn/data/rfmw74/h7X24hopperUni1k-regular-sphere-mesh10-particle24
+exp4o=/ddn/data/rfmw74/h7X25hopperUni1k-adaptive-sphere-mesh10-particle24
+exp5o=/ddn/data/rfmw74/h7X26hopperUni1k-reluctant-sphere-mesh10-particle24
 
-exp0="./dem-3d-release-tbb 0.1 0.001 0.1 hopperUniform1k 1000 regular-grid 0.00003 never 10 true sphere 10 2"
-exp1="./dem-3d-release-tbb 0.1 0.001 0.1 hopperUniform1k 1000 adaptive-grid 0.00003 never 10 true sphere 10 2"
-exp2="./dem-3d-release-tbb 0.1 0.001 0.1 hopperUniform1k 1000 reluctant-adaptive-grid 0.00003 never 10 true sphere 10 2"
-exp3="./dem-3d-release-omp-particle 0.1 0.001 0.1 hopperUniform1k 1000 regular-grid 0.00003 never 10 true sphere 10"
-exp4="./dem-3d-release-omp-particle 0.1 0.001 0.1 hopperUniform1k 1000 adaptive-grid 0.00003 never 10 true sphere 10"
-exp5="./dem-3d-release-omp-particle 0.1 0.001 0.1 hopperUniform1k 1000 reluctant-adaptive-grid 0.00003 never 10 true sphere 10"
+exp0="./dem-3d-release-tbb 0.1 0.001 0.1 hopperUniform1k 1000 regular-grid 0.00003 never 10 true sphere 10 24"
+exp1="./dem-3d-release-tbb 0.1 0.001 0.1 hopperUniform1k 1000 adaptive-grid 0.00003 never 10 true sphere 10 24"
+exp2="./dem-3d-release-tbb 0.1 0.001 0.1 hopperUniform1k 1000 reluctant-adaptive-grid 0.00003 never 10 true sphere 10 24"
+exp3="./dem-3d-release-omp-particle 0.1 0.001 0.1 hopperUniform1k 1000 regular-grid 0.00003 never 10 true sphere 10 24"
+exp4="./dem-3d-release-omp-particle 0.1 0.001 0.1 hopperUniform1k 1000 adaptive-grid 0.00003 never 10 true sphere 10 24"
+exp5="./dem-3d-release-omp-particle 0.1 0.001 0.1 hopperUniform1k 1000 reluctant-adaptive-grid 0.00003 never 10 true sphere 10 24"
 
 sed "s,{OUTPUT_FILE},$exp0o,g" ${JOB_SCRIPT0} > ${JOB_SCRIPT0}_tmp
 sed "s,{RUN},$exp0,g" ${JOB_SCRIPT0}_tmp > ${JOB_SCRIPT0}_tmp1
