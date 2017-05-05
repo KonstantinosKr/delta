2f97352da412246fc88f44fc037d60b883411c20
konstantinoskr <contact@ikonstantinos.com>
2017-05-05 02:07:55 +0100

    more renaming

--- delta_peano/compute/hamilton7_bro/hopper/1kUni/scale20/tbbomphybrids_short.sh
+++ delta_peano/compute/hamilton7_bro/hopper/1kUni/scale20/tbbomphybrids_short.sh
@@ -6,34 +6,20 @@
 # $1 path to the output file
 
 JOB_SCRIPT0=tbb1-omp24
-JOB_SCRIPT1=tbb1-omp24
-JOB_SCRIPT2=tbb24-omp1
+JOB_SCRIPT1=tbb24-omp1
+
+JOB_SCRIPT2=tbb1-omp24
 JOB_SCRIPT3=tbb24-omp1
 
-JOB_SCRIPT4=tbb1-omp24
-JOB_SCRIPT5=tbb1-omp24
-JOB_SCRIPT6=tbb24-omp1
-JOB_SCRIPT7=tbb24-omp1
-
 exp0o=/ddn/data/rfmw74/h7regularhybrid-on-triangle-pairsTBB_1_OMP_24_TRIANGLE_20_1000hopper
-exp1o=/ddn/data/rfmw74/h7regularhybrid-on-triangle-pairsTBB_1_OMP_24_TRIANGLE_20_1000hopper
-exp2o=/ddn/data/rfmw74/h7regularhybrid-on-triangle-pairsTBB_24_OMP_1_TRIANGLE_20_1000hopper
-exp3o=/ddn/data/rfmw74/h7regularhybrid-on-triangle-pairsTBB_24_OMP_1_TRIANGLE_20_1000hopper
+exp1o=/ddn/data/rfmw74/h7regularhybrid-on-triangle-pairsTBB_24_OMP_1_TRIANGLE_20_1000hopper
+exp2o=/ddn/data/rfmw74/h7regularhybrid-on-batchesTBB_1_OMP_24_TRIANGLE_20_1000hopper
+exp3o=/ddn/data/rfmw74/h7regularhybrid-on-batchesTBB_24_OMP_1_TRIANGLE_20_1000hopper
 
-exp4o=/ddn/data/rfmw74/h7regularhybrid-on-batchesTBB_1_OMP_24_TRIANGLE1_20_1000hopper
-exp5o=/ddn/data/rfmw74/h7regularhybrid-on-batchesTBB_1_OMP_24_TRIANGLE2_20_1000hopper
-exp6o=/ddn/data/rfmw74/h7regularhybrid-on-batchesTBB_24_OMP_1_TRIANGLE3_20_1000hopper
-exp7o=/ddn/data/rfmw74/h7regularhybrid-on-batchesTBB_24_OMP_1_TRIANGLE4_20_1000hopper
-
-exp0="./dem-3d-release-tbb-omp-triangle 0.1 0.001 0.1 hopperUniform1k 1000 regular-grid 0.00003 never 10 true hybrid-on-triangle-pairs 40 1"
-exp1="./dem-3d-release-tbb-omp-triangle 0.1 0.001 0.1 hopperUniform1k 1000 regular-grid 0.00003 never 10 true hybrid-on-triangle-pairs 40 1"
-exp2="./dem-3d-release-tbb-omp-triangle 0.1 0.001 0.1 hopperUniform1k 1000 regular-grid 0.00003 never 10 true hybrid-on-triangle-pairs 40 24"
-exp3="./dem-3d-release-tbb-omp-triangle 0.1 0.001 0.1 hopperUniform1k 1000 regular-grid 0.00003 never 10 true hybrid-on-triangle-pairs 40 24"
-
-exp4="./dem-3d-release-tbb-omp-triangle 0.1 0.001 0.1 hopperUniform1k 1000 regular-grid 0.00003 never 10 true hybrid-on-batches 40 1"
-exp5="./dem-3d-release-tbb-omp-triangle 0.1 0.001 0.1 hopperUniform1k 1000 regular-grid 0.00003 never 10 true hybrid-on-batches 40 1"
-exp6="./dem-3d-release-tbb-omp-triangle 0.1 0.001 0.1 hopperUniform1k 1000 regular-grid 0.00003 never 10 true hybrid-on-batches 40 24"
-exp7="./dem-3d-release-tbb-omp-triangle 0.1 0.001 0.1 hopperUniform1k 1000 regular-grid 0.00003 never 10 true hybrid-on-batches 40 24"
+exp0="./dem-3d-release-tbb-omp-triangle 0.1 0.001 0.1 hopperUniform1k 1000 regular-grid 0.00003 never 10 true hybrid-on-triangle-pairs 20 1"
+exp1="./dem-3d-release-tbb-omp-triangle 0.1 0.001 0.1 hopperUniform1k 1000 regular-grid 0.00003 never 10 true hybrid-on-triangle-pairs 20 24"
+exp2="./dem-3d-release-tbb-omp-triangle 0.1 0.001 0.1 hopperUniform1k 1000 regular-grid 0.00003 never 10 true hybrid-on-batches 20 1"
+exp3="./dem-3d-release-tbb-omp-triangle 0.1 0.001 0.1 hopperUniform1k 1000 regular-grid 0.00003 never 10 true hybrid-on-batches 20 24"
 
 sed "s,{OUTPUT_FILE},$exp0o,g" ${JOB_SCRIPT0} > ${JOB_SCRIPT0}_tmp
 sed "s,{RUN},$exp0,g" ${JOB_SCRIPT0}_tmp > ${JOB_SCRIPT0}_tmp1
@@ -62,31 +48,3 @@ sbatch ${JOB_SCRIPT3}_tmp1
 
 rm ${JOB_SCRIPT3}_tmp
 rm ${JOB_SCRIPT3}_tmp1
-
-sed "s,{OUTPUT_FILE},$exp4o,g" ${JOB_SCRIPT4} > ${JOB_SCRIPT4}_tmp
-sed "s,{RUN},$exp4,g" ${JOB_SCRIPT4}_tmp > ${JOB_SCRIPT4}_tmp1
-sbatch ${JOB_SCRIPT4}_tmp1
-
-rm ${JOB_SCRIPT4}_tmp
-rm ${JOB_SCRIPT4}_tmp1
-
-sed "s,{OUTPUT_FILE},$exp5o,g" ${JOB_SCRIPT5} > ${JOB_SCRIPT5}_tmp
-sed "s,{RUN},$exp5,g" ${JOB_SCRIPT5}_tmp > ${JOB_SCRIPT5}_tmp1
-sbatch ${JOB_SCRIPT5}_tmp1
-
-rm ${JOB_SCRIPT5}_tmp
-rm ${JOB_SCRIPT5}_tmp1
-
-sed "s,{OUTPUT_FILE},$exp6o,g" ${JOB_SCRIPT6} > ${JOB_SCRIPT6}_tmp
-sed "s,{RUN},$exp6,g" ${JOB_SCRIPT6}_tmp > ${JOB_SCRIPT6}_tmp1
-sbatch ${JOB_SCRIPT6}_tmp1
-
-rm ${JOB_SCRIPT6}_tmp
-rm ${JOB_SCRIPT6}_tmp1
-
-sed "s,{OUTPUT_FILE},$exp7o,g" ${JOB_SCRIPT7} > ${JOB_SCRIPT7}_tmp
-sed "s,{RUN},$exp7,g" ${JOB_SCRIPT7}_tmp > ${JOB_SCRIPT7}_tmp1
-sbatch ${JOB_SCRIPT7}_tmp1
-
-rm ${JOB_SCRIPT7}_tmp
-rm ${JOB_SCRIPT7}_tmp1
