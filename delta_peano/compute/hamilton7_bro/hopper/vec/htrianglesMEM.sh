64b728b3804426da466a6583ab7cbadad9690e8a
konstantinoskr <contact@ikonstantinos.com>
2017-06-22 04:05:58 +0100

    reduced steps for vec reports

--- delta_peano/compute/hamilton7_bro/hopper/vec/htrianglesMEM.sh
+++ delta_peano/compute/hamilton7_bro/hopper/vec/htrianglesMEM.sh
@@ -9,7 +9,7 @@ JOB_SCRIPT0=runjobBatch
 
 exp0o=/ddn/data/rfmw74/h7regularhybrid-on-triangle-pairsVecMEMHopper1k
 
-exp0="likwid-perfctr -C 1 -g MEM ./dem-3d-release-vec 0.1 0.001 0.1 hopperUniform1k 1000 regular-grid 0.0001 never 10 true hybrid-on-triangle-pairs 40"
+exp0="likwid-perfctr -C 1 -g MEM ./dem-3d-release-vec 0.1 0.001 0.1 hopperUniform1k 25 regular-grid 0.0001 never 10 true hybrid-on-triangle-pairs 40"
 
 sed "s,{OUTPUT_FILE},$exp0o,g" ${JOB_SCRIPT0} > ${JOB_SCRIPT0}_tmp
 sed "s,{RUN},$exp0,g" ${JOB_SCRIPT0}_tmp > ${JOB_SCRIPT0}_tmp1
