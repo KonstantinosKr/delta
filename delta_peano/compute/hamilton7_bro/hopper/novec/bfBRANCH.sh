046459a7fb3526f29cdb05461dd7c15614a1b342
konstantinoskr <contact@ikonstantinos.com>
2017-06-26 18:45:05 +0100

    no vec likwid

--- delta_peano/compute/hamilton7_bro/hopper/novec/bfBRANCH.sh
+++ delta_peano/compute/hamilton7_bro/hopper/novec/bfBRANCH.sh
@@ -7,9 +7,9 @@
 
 JOB_SCRIPT0=runjobBatch
 
-exp0o=/ddn/data/rfmw74/h7regularhybrid-on-triangle-pairsnovecMEMHopper1k
+exp0o=/ddn/data/rfmw74/h7regularbfnovecBRANCHHopper1k
 
-exp0="likwid-perfctr -f -C 1 -g MEM ./dem-3d-release-novec 0.1 0.001 0.1 hopperUniform1k 25 regular-grid 0.0001 never 10 true hybrid-on-triangle-pairs 40"
+exp0="likwid-perfctr -f -C 1 -g BRANCH ./dem-3d-release-novec 0.1 0.001 0.1 hopperUniform1k 25 regular-grid 0.0001 never 10 true bf 40"
 
 sed "s,{OUTPUT_FILE},$exp0o,g" ${JOB_SCRIPT0} > ${JOB_SCRIPT0}_tmp
 sed "s,{RUN},$exp0,g" ${JOB_SCRIPT0}_tmp > ${JOB_SCRIPT0}_tmp1
