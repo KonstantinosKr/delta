5de27115a05f1c63145408ccb96b08ef13a1e0ab
Konstantinos Krestenitis <contact@ikonstantinos.com>
2018-02-12 18:13:57 +0000

    insignificant changes

--- delta_peano/delta/collision/penalty.h
+++ delta_peano/delta/collision/penalty.h
@@ -85,7 +85,7 @@ namespace delta {
 
     #if defined(ompParticle) || defined(ompTriangle)
       #pragma omp declare simd
-      #pragma omp nomask notinbranch declare simd linear(xCoordinatesOfTriangleA:3) linear(yCoordinatesOfTriangleA:3) linear(zCoordinatesOfTriangleA:3) linear(xCoordinatesOfTriangleB:3) linear(yCoordinatesOfTriangleB:3) linear(zCoordinatesOfTriangleB:3)
+      #pragma omp declare simd linear(xCoordinatesOfTriangleA:3) linear(yCoordinatesOfTriangleA:3) linear(zCoordinatesOfTriangleA:3) linear(xCoordinatesOfTriangleB:3) linear(yCoordinatesOfTriangleB:3) linear(zCoordinatesOfTriangleB:3) nomask notinbranch
     #endif
     extern void penalty(
       const iREAL   *xCoordinatesOfTriangleA,
